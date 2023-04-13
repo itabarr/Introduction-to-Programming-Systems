38d6d8168aec50fc7fcf6967d04570bd9e4e050e
Itamar Barron <itabarr825@gmail.com>
2023-04-14 00:43:10 +0300

    final fixes

--- main.c
+++ main.c
@@ -20,11 +20,13 @@ void read_command(char *cmd , char *parsed_cmd);
 void exit_program(int *run, char *cmd, process_manager *ph);
 void change_directory(char *cmd , char *args[]);
 int parse_command(char *cmd, char *args[], int *is_background, int *is_internal, int *redirect, char *redirect_file);
-void print_jobs(char *cmd , process_manager *ph);
+void print_jobs(char *args[] , process_manager *ph);
 int is_valid_dir(char *dir);
 int is_internal_command(char *cmd);
 int is_background_command(char *last_arg);
 void run_external_process(char cmd[], char *args[], int is_background , process_manager *ph , int redirect , char *redirect_file);
+void run_internal_process(char cmd[], char *args[], int *run, process_manager *ph , int is_background , int redirect , char *redirect_file);
+void handle_redirect(char *args[], int *redirect , char *redirect_file);
 // void handle_background(char *cmd); 
 
 int main() {
@@ -53,9 +55,7 @@ int main() {
         }
 
         if (is_internal) {
-            exit_program(&run, args[0] , ph);
-            change_directory(args[0], args);
-            print_jobs(args[0] , ph);
+            run_internal_process(cmd, args, &run, ph , is_background , redirect , redirect_file);
         }
         else{
             run_external_process(cmd, args, is_background , ph, redirect, redirect_file);
@@ -67,7 +67,7 @@ int main() {
 }
 
 // prints prompt to console
-void print_prompt(void) {
+void print_prompt() {
     printf("hw1shell$ "); 
 }
 
@@ -92,6 +92,11 @@ int parse_command(char *cmd, char *args[] , int *is_background, int *is_internal
     }
     args[i] = NULL;
 
+    if (args[0] == NULL) {
+        *is_internal = 1;
+        return 1;
+    }
+
     *is_background = is_background_command(args[i-1]);
     *is_internal = is_internal_command(args[0]);
 
@@ -104,13 +109,13 @@ int parse_command(char *cmd, char *args[] , int *is_background, int *is_internal
     // check if there is a redirect
     handle_redirect(args, redirect, redirect_file);
 
-    if (*redirect == 1 && *redirect_file != NULL) {
+    if (*redirect == 1 && *redirect_file != '\0') {
         // remove 2 last args
         args[i - 1] = NULL;
         args[i - 2] = NULL;
 
     }
-    if (*redirect == 1 && *redirect_file == NULL) {
+    if (*redirect == 1 && *redirect_file == '\0') {
         return 0;
     }
     return 1;
@@ -118,7 +123,7 @@ int parse_command(char *cmd, char *args[] , int *is_background, int *is_internal
 
 // checks if command is a internal command
 int is_internal_command(char *cmd) {
-    if (strcmp(cmd, "exit") == 0 || strcmp(cmd, "cd") == 0 || strcmp(cmd, "jobs") == 0) {
+    if (cmd == NULL || strcmp(cmd, "exit") == 0 || strcmp(cmd, "cd") == 0 || strcmp(cmd, "jobs") == 0) {
         return 1;
     }
     return 0;
@@ -150,14 +155,7 @@ void change_directory(char *cmd , char *args[]){
     int result;
     if (strcmp(cmd, "cd") == 0) {
 
-        if (args[1] == NULL) {
-            printf("hw1shell: invalid command\n");
-        }
-        else if (strcmp(args[1], "..") == 0) {
-            chdir("..");
-        }
-
-        else if (!is_valid_dir(args[1])) {
+        if (!is_valid_dir(args[1])) {
             printf("hw1shell: invalid command\n");
         }
 
@@ -175,12 +173,28 @@ void change_directory(char *cmd , char *args[]){
 
 //function that checks if str is a valid directory format
 int is_valid_dir(char *str){
-    int i = 0;
-    while(str[i] != '\0'){
-        if(str[i] == '/'){
+    int i;
+    int len;
+
+    if (str == NULL){
+    return 0;
+    }
+    // if .. than ok
+    if (strcmp(str, "..") == 0){
+        return 1;
+    }
+    
+    // if str is null or - or ~ or . then not valid (by question definition)
+    if (str == NULL || strcmp(str, "-") == 0 || strcmp(str, "~") == 0 || strcmp(str, ".") == 0){
+        return 0;
+    }
+
+    // if str is combination of . and / then not valid (cmdir will try to open it)
+    len = strlen(str);
+    for (i = 0; i < len; i++) {
+        if (str[i] != '.' && str[i] != '/') {
             return 1;
         }
-        i++;
     }
     return 0;
 }
@@ -189,10 +203,11 @@ int is_valid_dir(char *str){
 void run_external_process(char cmd[], char *args[], int is_background, process_manager *ph , int redirect , char *redirect_file) {
     pid_t pid;
     int status;
-    FILE *fp;
+    FILE *fp = NULL;
     int fd; 
     if (is_background && is_full(ph)){
         printf("hw1shell: too many background commands running\n");
+        clean_finished_processes(ph);
         return;
 
     }
@@ -239,9 +254,89 @@ void run_external_process(char cmd[], char *args[], int is_background, process_m
 
 }
 
+// run internal process
+void run_internal_process(char cmd[], char *args[], int *run,  process_manager *ph, int is_background, int redirect , char *redirect_file) {
+    pid_t pid;
+    //int status;
+    FILE *fp = NULL;
+    int fd; 
+    int original_stdout = dup(STDOUT_FILENO);
+
+    // empty command, continue
+    if (args[0] == NULL) {
+        clean_finished_processes(ph);
+        return;
+    }
+
+    // exit and other commands with args not allowed
+    if ((strcmp(args[0], "exit") == 0)   && ( args[1] != NULL || redirect || is_background)) {
+        printf("hw1shell: invalid command\n");
+        clean_finished_processes(ph);
+        return;
+    }
+
+    exit_program(run, args[0] , ph);
+    
+    // redirect output if redirect
+    if (redirect){
+        fp = fopen(redirect_file, "w");
+        fd = fileno(fp);
+    }
+
+    // run forward command (and handle redirect)
+    if (!is_background){
+        if (redirect){
+                dup2(fd, STDOUT_FILENO);
+            }
+
+        change_directory(args[0], args);
+        print_jobs(args , ph);
+
+        if (redirect){
+                fclose(fp);
+                dup2(original_stdout, STDOUT_FILENO);
+            }
+        
+        clean_finished_processes(ph);
+        return;
+
+    }
+
+    // run background command
+    else{
+        pid = fork();
+
+        if (pid == 0) {
+            if (redirect){
+                dup2(fd, STDOUT_FILENO);
+            }
+            change_directory(args[0], args);
+            print_jobs(args , ph);
+            exit(0);
+
+        } else if (pid > 0) {
+            // Parent process
+            printf("hw1shell: pid %d started\n", pid);
+            add_process(ph, pid , cmd , fp);
+        } else {
+            // Fork failed
+            perror("fork() failed");
+            exit(1);
+        }
+    }
+    clean_finished_processes(ph);
+    
+}
+
 // prints the jobs if command is jobs
-void print_jobs(char *cmd, process_manager *ph){
-    if(strcmp(cmd,"jobs") == 0){
+void print_jobs(char *args[], process_manager *ph){
+    if(strcmp(args[0],"jobs") == 0){
+
+        if (args[1] != NULL){
+            printf("hw1shell: invalid command\n");
+            return;
+        }
+
         print_process_manager(ph);
     }
     return;
