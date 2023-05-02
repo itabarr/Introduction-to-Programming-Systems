b76fad7f782b79763e3f653071400aa45863d2a7
Itamar Barron <itabarr825@gmail.com>
2023-05-02 20:51:56 +0300

    remove empty files creation + long long cound + cleaning

--- basic_commands.c
+++ basic_commands.c
@@ -7,39 +7,30 @@ void msleep(int x) {
 
 void increment(int x) {
     char filename[16];
-    sprintf(filename, "counter%d.txt", x);
-
+    sprintf(filename,  "count%02d.txt", x);
     FILE *fp = fopen(filename, "r+");
-    if (fp == NULL) {
-        fp = fopen(filename, "w+");
-        fprintf(fp, "0");
-        fseek(fp, 0, SEEK_SET);
-    }
 
-    int count;
-    fscanf(fp, "%d", &count);
+    long long count;
+    fscanf(fp, "%lld", &count);
     count++;
     fseek(fp, 0, SEEK_SET);
-    fprintf(fp, "%d", count);
+    fprintf(fp, "%lld", count);
+
     fclose(fp);
 }
 
 void decrement(int x) {
     char filename[16];
-    sprintf(filename, "counter%d.txt", x);
+    sprintf(filename, "count%02d.txt", x);
 
     FILE *fp = fopen(filename, "r+");
-    if (fp == NULL) {
-        fp = fopen(filename, "w+");
-        fprintf(fp, "0");
-        fseek(fp, 0, SEEK_SET);
-    }
 
-    int count;
-    fscanf(fp, "%d", &count);
+    long long count;
+    fscanf(fp, "%lld", &count);
     count--;
     fseek(fp, 0, SEEK_SET);
-    fprintf(fp, "%d", count);
+    fprintf(fp, "%lld", count);
+
     fclose(fp);
 }
 
@@ -111,47 +102,3 @@ void run_job(void *_commands_str) {
         }
     }
 }
-
-// int main() {
-//     // Test case 1: Basic sequence of commands
-//     char *commands_str_1 = "increment 1;msleep 2;decrement 1";
-//     printf("Test case 1: Basic sequence of commands\n");
-//     printf("Before: Counter1.txt = ");
-//     FILE *fp1 = fopen("counter1.txt", "r");
-//     int count1;
-//     if (fp1 != NULL) {
-//         fscanf(fp1, "%d", &count1);
-//         fclose(fp1);
-//     }
-//     printf("%d\n", count1);
-//     run_job(commands_str_1);
-//     printf("After: Counter1.txt = ");
-//     fp1 = fopen("counter1.txt", "r");
-//     if (fp1 != NULL) {
-//         fscanf(fp1, "%d", &count1);
-//         fclose(fp1);
-//     }
-//     printf("%d\n", count1);
-
-//     // Test case 2: Repeat a sequence of commands
-//     char *commands_str_2 = "repeat 3;increment 2;msleep 5;decrement 1";
-//     printf("Test case 2: Repeat a sequence of commands\n");
-//     printf("Before: Counter2.txt = ");
-//     FILE *fp2 = fopen("counter2.txt", "r");
-//     int count2;
-//     if (fp2 != NULL) {
-//         fscanf(fp2, "%d", &count2);
-//         fclose(fp2);
-//     }
-//     printf("%d\n", count2);
-//     run_job(commands_str_2);
-//     printf("After: Counter2.txt = ");
-//     fp2 = fopen("counter2.txt", "r");
-//     if (fp2 != NULL) {
-//         fscanf(fp2, "%d", &count2);
-//         fclose(fp2);
-//     }
-//     printf("%d\n", count2);
-
-//     return 0;
-// }
